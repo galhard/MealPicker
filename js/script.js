dafd388658613a25f21e8738b78aeda084391651
galhard <93837672+galhard@users.noreply.github.com>
2022-02-25 23:52:56 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -325,7 +325,7 @@ meals.forEach(function (value1, key, _) {
         value2.title
       }</p> </div>`
     );
-    //Opening Meal Max Card
+    //#Opening Meal Max Card
     const mealCards = document.querySelector(`.${key}__container .meal`);
     const infoBtn = mealCards.querySelector('.infoIcon__card-min');
     const body = document.querySelector('body');
@@ -344,7 +344,6 @@ meals.forEach(function (value1, key, _) {
       let allIngredients = combinedIngredients(value2.ingredients.grams)
         .concat(combinedIngredients(value2.ingredients.nonGrams))
         .join('\n');
-      //const valueduplicate = `${value[0] * 2} ${value[1]}`; Przy dodawaniu kolejnych osób
 
       document.querySelector('.mealMax-bcblack').style.display = 'block';
       body.insertAdjacentHTML(
@@ -357,22 +356,25 @@ meals.forEach(function (value1, key, _) {
           value2.recipe
         }</p> </div> </div> </div>`
       );
-      //CLosing Card Max
+      //##Closing Card Max
       const closeBtn = document.querySelector('.closeIcon__card-max');
-      closeBtn.addEventListener('click', function () {
-        document.querySelector('.mealMax-fixed').remove();
-        document.querySelector('.mealMax-bcblack').style.display = 'none';
-      });
-      //Adding/Removing Person & calculating ingredients value
+      function closeMealMaxCard(btn) {
+        btn.addEventListener('click', function () {
+          document.querySelector('.mealMax-fixed').remove();
+          document.querySelector('.mealMax-bcblack').style.display = 'none';
+        });
+      }
+      closeMealMaxCard(closeBtn);
+      //##Adding/Removing Person & calculating ingredients value
       const addPerson = document.querySelector('.addPersonIcon__card-max');
       const removePerson = document.querySelector(
         '.removePersonIcon__card-max'
       );
-      //Persons quantity - needed to multiple ingredients value
+      //###Persons quantity - needed to multiple ingredients value
       let nrOfPersons = document.querySelector(
         '.mealMax__info__persons__number'
       );
-      //Function creating new ingredients string; I will try to shorten this function
+      //###Function creating new ingredients string; I will try to shorten this function
       function newIngredientString() {
         let nonGramsIng = [];
         value2.ingredients.nonGrams.forEach(function (value, keys, map) {
@@ -396,15 +398,16 @@ meals.forEach(function (value1, key, _) {
         document.querySelector('.mealMax__ingredients__content').textContent =
           allIngredientsNew;
       }
-      //Adding Person & changing ingredients quantity
+      //###Adding Person & changing ingredients quantity
       addPerson.addEventListener('click', function () {
         nrOfPersons.textContent = nrOfPersons.textContent * 1 + 1;
         nrOfPersons.textContent === '1'
           ? (removePerson.style.display = 'none')
           : (removePerson.style.display = 'block');
         newIngredientString();
+        console.log(nrOfPersons.textContent);
       });
-      //Removing Person & changing ingredients quantity
+      //###Removing Person & changing ingredients quantity
       removePerson.addEventListener('click', function () {
         nrOfPersons.textContent = nrOfPersons.textContent * 1 - 1;
         nrOfPersons.textContent === '1'
@@ -412,6 +415,114 @@ meals.forEach(function (value1, key, _) {
           : (removePerson.style.display = 'block');
         newIngredientString();
       });
+      const addMealMaxToList = document.querySelector('.addIcon__card-max');
+      const listFirstMealEl = document.querySelectorAll('.firstMeal');
+      const listSecondMealEl = document.querySelectorAll('.secondMeal');
+      const listThirdMealEl = document.querySelectorAll('.thirdMeal');
+      const listCounterCurrent = document.querySelector('.list__counter__curr');
+      const listAddMeal = function (mealType) {
+        addMealMaxToList.addEventListener('click', function () {
+          //Creating array containing all empty (without text content) elements of a given type (firstMeal, secondMeal or thirdMeal)
+          const emptyMeal = [];
+
+          for (const meal of mealType) {
+            if (meal.textContent === '') {
+              emptyMeal.push(meal);
+            }
+          }
+          //Checking if created array is empty; preventing from error of adding more than 7 meals of a given type
+          if (emptyMeal.length < 1) {
+            alert('You selected maximum number of meal this type');
+          } else {
+            //ADD Meal Title to List, Del Icon Visible, Channging Picked Meals Number in Counter
+            //3 STEPS below is better than changing emptyMeal[0] textContent, this prevents from later bugs with counter update
+            const listMealSpan = document.createElement('span'); //1st STEP
+            listMealSpan.textContent = value2.title; //2nd STEP
+            emptyMeal[0].appendChild(listMealSpan); //3rd STEP; here emptyMeal stop being empty
+            emptyMeal[0].parentNode.insertAdjacentHTML(
+              'beforeend',
+              '<ion-icon class="removeIcon__list" name="close"></ion-icon>'
+            );
+            emptyMeal[0].nextElementSibling.style.display = 'block'; //showing removing icon that can be used to remove meal from list
+            listCounterCurrent.textContent =
+              Number(listCounterCurrent.textContent) + 1; //updating counter each time meal is added
+          }
+          //LIST
+          //When CLICK on the name of a meal in the list, the Meal Max Card of that particular meal is shown, including changing the amount of ingredients
+          const nonEmptyMeal = emptyMeal[0]; //I may not created this variable but it helps me to better understand what's going on
+          nonEmptyMeal?.addEventListener('click', function () {
+            document.querySelector('.mealMax-bcblack').style.display = 'block';
+            body.insertAdjacentHTML(
+              'afterbegin',
+              `<div class="mealMax-fixed"> <div class="mealMax"> <ion-icon class="closeIcon__card-max" name="close-outline"></ion-icon> <div class="mealMax__title"> <h4> ${
+                value2.title
+              } </h4> </div> <div class="mealMax__info"> <div class="mealMax__info__persons"><span class="mealMax__info__persons__number">${
+                nrOfPersons.textContent
+              }</span class="mealMax__info__persons__text"><span>os.</span></div> <div class="mealMax__info__time"> <ion-icon class="timeIcon__card-max" name="hourglass-outline" ></ion-icon> <span class="mealMax__info__time__text">${`'${value2.time}`}</span> </div> <p class="mealMax__info__kcal">${
+                value2.kcal
+              } kcal</p> </div> <img class="mealMax__img" src="img/meal.jpg" alt="" /> <div class="mealMax__ingredients"> <p class="mealMax__ingredients__title">składniki:</p> <p class="mealMax__ingredients__content">${allIngredients}</p> </div> <div class="mealMax__recipe"> <p class="mealMax__recipe__title">Przygotowanie:</p> <p class="mealMax__recipe__content">${
+                value2.recipe
+              }</p> </div> </div> </div>`
+            );
+
+            newIngredientString(); //Function which change ingredients text
+            //Close Meal Max Card
+            //Creating variable needed to use in function created earlier. I can't use 'closeBtn' because different Btn was created in this scope.
+            const closeBtnListMealMaxCard = document.querySelector(
+              '.closeIcon__card-max'
+            ); //Creating variable needed to use in function created earlier. I can't use 'closeBtn' because different Btn was created in this scope.
+            closeMealMaxCard(closeBtnListMealMaxCard); //Function closing Meal Max Card which was created in this scope
+          });
+          //Removing meal from list
+          const listRemoveIcons =
+            document.querySelectorAll('.removeIcon__list');
+          // console.log(listRemoveIcons);
+          let iconArr = [];
+          listRemoveIcons.forEach(function (icon) {
+            iconArr.push(icon);
+          });
+
+          console.log(iconArr);
+
+          iconArr.forEach(function (el) {
+            el.addEventListener(
+              'click',
+              function () {
+                console.log(el.parentNode);
+              },
+              { once: true }
+            );
+          });
+
+          // iconArr.map(function (value, index, array) {
+          //   console.log(value);
+          //   value.addEventListener('click', function () {
+          //     console.log(value.splice());
+          //   });
+          // });
+
+          // for (const icon of iconArr) {
+          //   icon.map(function (value, index, array) {
+          //     console.log(value);
+          //   });
+
+          //   icon.addEventListener('click', function () {
+          //     console.log(icon);
+
+          //     // icon.style.display = 'none';
+          //     // listCounterCurrent.textContent =
+          //     //   Number(listCounterCurrent.textContent) - 1;
+          //   });
+          // }
+        });
+      };
+      if (key === 'firstMeal') {
+        listAddMeal(listFirstMealEl);
+      } else if (key === 'secondMeal') {
+        listAddMeal(listSecondMealEl);
+      } else if (key === 'thirdMeal') {
+        listAddMeal(listThirdMealEl);
+      }
     });
   });
 });
@@ -450,11 +561,11 @@ listClose.addEventListener('click', function () {
 //COUNTER
 
 //Number of children in every days of the week
-const listDayContainers = document.querySelectorAll('.list__day__container');
-let listNrOfChildren = 0;
-listDayContainers.forEach(day => {
-  const elNumber = day.childElementCount;
-  return (listNrOfChildren += elNumber);
-});
-//Insert number of children to Counter
-document.querySelector('.list__counter').textContent = `${listNrOfChildren}/21`;
+// const listDayContainers = document.querySelectorAll('.list__day__container');
+// let listNrOfChildren = 0;
+// listDayContainers.forEach(day => {
+//   const elNumber = day.childElementCount;
+//   return (listNrOfChildren += elNumber);
+// });
+// //Insert number of children to Counter
+// document.querySelector('.list__counter').textContent = `${listNrOfChildren}/21`;
